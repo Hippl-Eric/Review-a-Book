f051d3ee1f36f2ca09f3edf4d1b779c3fcd6dfd2
Eric Hippler <hippler.eric@gmail.com>
2020-06-19 09:14:10 -0400

    DB access working, login required wrapper and register working

--- application.py
+++ application.py
@@ -1,13 +1,15 @@
 import os
 import requests
 
-from flask import Flask, session
+from flask import Flask, session, render_template, redirect, url_for, request
 from flask_session import Session
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.orm import scoped_session, sessionmaker
 from dotenv import load_dotenv
+from helpers import login_required
 
 app = Flask(__name__)
+app.secret_key = os.urandom(16)
 
 #import enviroment varibles from .env file
 load_dotenv()
@@ -25,13 +27,75 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(DATABASE_URL)
 db = scoped_session(sessionmaker(bind=engine))
 
 #test access to API
-res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": API_KEY, "isbns": "9781632168146"})
-print(res.json())
+# res = requests.get("https://www.goodreads.com/book/review_counts.json", params={"key": API_KEY, "isbns": "9781632168146"})
+# print(res.json())
+
+# test access to db
+# all_users = db.execute("SELECT * FROM users WHERE id = 2").first()
+# print(all_users)
 
 @app.route("/")
+@login_required
 def index():
-    return "Project 1: TODO"
+    userID = session.get("user_id")
+    return render_template("index.html", userID=userID)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+
+        # Parse form input from user
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        # Check if username and password in db
+
+            # If true store user_id in session
+            # redirect to index
+            # If false return error message
+
+        return f"username = {username}, password = {password}"
+
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+
+        session.pop('user_id', None)
+
+        # Parse form input from user
+        user_name = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        # Check database if username is already taken
+        # 1:34 in lecture 3
+        user_check = db.execute("SELECT * FROM users WHERE username=:username", {"username":user_name}).fetchall()
+
+        # Return error message
+        if user_check:
+            return "username already taken"
+        
+        # Add user to db, store user_id in session, redirect to index page
+        else:
+            db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": user_name, "password": password})
+            db.commit()
+            user_id = db.execute("SELECT id FROM users WHERE username=:username", {"username": user_name}).fetchall()
+            all_users = db.execute("SELECT * FROM users").fetchall()
+            session["user_id"] = user_id[0]
+            return redirect(url_for("index"))
+
+
+    return render_template("register.html")
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
